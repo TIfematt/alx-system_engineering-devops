0a38fe70c7a8f087013420f9a02e32b9f6587631
Boluwatife <boluwatifeoladejo@gmail.com>
2023-03-01 19:22:32 +0100

    added first API call

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-"""A function that queries reddit API"""
+"""
+Function that queries the Reddit API and returns
+the number of subscribers for a given subreddit.
+"""
 import requests
 import sys
 
+
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API"""
+    """ Queries to Reddit API """
     u_agent = 'Mozilla/5.0'
-    
+
     headers = {
         'User-Agent': u_agent
     }
-    
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     res = requests.get(url, headers=headers, allow_redirects=False)
     if res.status_code != 200:
